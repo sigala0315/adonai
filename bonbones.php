3fce2b30272225ece4324ef13243a8de35389770
Manolero232 <115741146+Manolero232@users.noreply.github.com>
2022-10-18 15:54:16 -0500

    Update bonbones.php

--- bonbones.php
+++ bonbones.php
@@ -1,3 +1,10 @@
 <?php
-$ con = mysqli__connect('127.0.0.1','root','','planeto_tierra_sigala')
+    $con=mysqli_connect('127.0.0.1','root','','planeto_tierra_jose');
+    error_reporting(0);
+    if(!$con){
+        echo('<p>tu conexion falla</p>');
+    }else{
+        echo('<p>tu conexion es exitosa</p>');
+    }
+
 ?>
