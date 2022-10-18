37e9b37c679723bd47bd1362f92da83c45450587
Manolero232 <115741146+Manolero232@users.noreply.github.com>
2022-10-18 15:05:11 -0500

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,4 +1,17 @@
 <¿php
       require_once('bonbones');
       
+      $Nombrep=$_Post['Nombre'];
+      $Capi=$_Post['Capital'];
+      $Superficie=$_Post['Superf'];
+      $Poblacion=$_Post['Poblac'];
+      $Idioma=$_Post['Idiota'];
+      $Moneda=$_Post['Peje'];
+
+      echo($Nombrep.'<br>');
+      echo($Capi.'<br>');
+      echo($Superficie.'<br>');
+      echo($Poblacion.'<br>');
+      echo($Idioma.'<br>');
+      echo($Moneda.'<br>');
 ?>
